d53b98a06ad684a3bfdb418029f4b1edec2f98e2
fabijannn <fabianuhrin1@gmail.com>
2025-02-12 16:22:41 +0100

    added files

---
+++ src/constants.py
@@ -0,0 +1,29 @@
+class Constants:
+    # Window size
+    def __init__(self):
+        pass
+
+    WIDTH, HEIGHT = 1024, 768
+
+    # Colors
+    WHITE = (255, 255, 255)
+    YELLOW = (255, 255, 0)
+    BLUE = (50, 50, 255)
+    RED = (255, 100, 50)
+    GREY = (150, 150, 150)
+    ORANGE = (195, 145, 25)
+    LIGHT_BEIGE = (216, 202, 157)
+    BEIGE = (226, 191, 125)
+    LIGHT_BLUE = (175, 219, 245)
+    DARK_BLUE = (2, 0, 121)
+
+    # Planetary masses (in kg)
+    SUN_MASS = 1.98892 * 10 ** 30
+    EARTH_MASS = 5.9742 * 10 ** 24
+    MARS_MASS = 6.3903 * 10 ** 23
+    VENUS_MASS = 4.8675 * 10 ** 24
+    MERCURY_MASS = 3.30 * 10 ** 23
+    JUPITER_MASS = 1.97 * 10 ** 27
+    SATURN_MASS = 5.683 * 10 ** 26
+    URANUS_MASS = 8.681 * 10 ** 25
+    NEPTUNE_MASS = 1.024 * 10 ** 26
